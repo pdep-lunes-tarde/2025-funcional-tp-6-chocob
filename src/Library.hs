48e8cd475f129a111915a6ca9a8ac3fa40f7a671
santimoretti <smorettimurillo@frba.utn.edu.ar>
2025-05-08 19:00:55 -0300

    precioFinal

--- src/Library.hs
+++ src/Library.hs
@@ -5,6 +5,7 @@ data Ingrediente =
     Carne | Pan | Panceta | Cheddar | Pollo | Curry | QuesoDeAlmendras
     deriving (Eq, Show)
 
+precioIngrediente :: Ingrediente -> Number
 precioIngrediente Carne = 20
 precioIngrediente Pan = 2
 precioIngrediente Panceta = 10
@@ -18,3 +19,11 @@ data Hamburguesa = Hamburguesa {
     ingredientes :: [Ingrediente]
 } deriving (Eq, Show)
 
+cuartoDeLibra :: Hamburguesa
+cuartoDeLibra = Hamburguesa { precioBase = 20, ingredientes = [Carne,Pan,Cheddar,Pan] }
+
+precioFinal :: Hamburguesa -> Number
+precioFinal hamburguesa = sumatoriaIngredientes hamburguesa + precioBase hamburguesa
+
+sumatoriaIngredientes :: Hamburguesa -> Number
+sumatoriaIngredientes hamburguesa = sum . map precioIngrediente $ ingredientes hamburguesa 
--- src/Spec.hs
+++ src/Spec.hs
@@ -6,6 +6,6 @@ import Control.Exception (evaluate)
 
 correrTests :: IO ()
 correrTests = hspec $ do
-    describe "TP 5" $ do
-        it "test de prueba" $ do
-            2 + 2 `shouldBe` 4
+    describe "precioFinal" $ do
+        it "El precio final de una hamburguesa es la sumatoria de los precios de los ingredientes + el precio base." $ do
+            precioFinal cuartoDeLibra `shouldBe` 54
