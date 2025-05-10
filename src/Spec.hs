48c2ad559ef7337173c9abcc57f25491fa08855b
NemeFranco <franconeme@gmail.com>
2025-05-10 19:53:45 -0300

    Tests Parte 3

--- src/Spec.hs
+++ src/Spec.hs
@@ -9,7 +9,7 @@ correrTests :: IO ()
 correrTests = hspec $ do
     suiteDeTestsParte1
     suiteDeTestsParte2
-    -- suiteDeTestsParte3
+    suiteDeTestsParte3
 
 suiteDeTestsParte1 = describe "Hamburguesas" $ do
     let mcPollo = Hamburguesa {precioBase = 20, ingredientes = [Pan,Pollo,QuesoDeAlmendras,Pan]}
@@ -34,7 +34,7 @@ suiteDeTestsParte1 = describe "Hamburguesas" $ do
         it "Dado un cuarto de libra y un descuento del 20% retorna un cuarto de libra con descuento aplicado, agrandada dos veces y con panceta y cheddar agregados" $ do
             pdepBurger `shouldBe` cuartoDeLibra {precioBase = 16, ingredientes = [Pan, Cheddar, Carne, Panceta, Carne, Carne, Cheddar, Pan]}
    
-suiteDeTestsParte2 = describe "Hamburguesas" $ do  
+suiteDeTestsParte2 = describe "Algunas hamburguesas mas" $ do  
     describe "dobleCuarto" $ do
         it "El precio final de la Doble Cuarto deberia ser 84 " $ do
             precioFinal dobleCuarto `shouldBe` 84
@@ -50,3 +50,20 @@ suiteDeTestsParte2 = describe "Hamburguesas" $ do
             delDia bigPdep `shouldBe` bigPdep {precioBase = 14, ingredientes = [Papas, Pan, Curry, Cheddar, Carne, Carne, Cheddar, Pan]}
         it "una Doble Cuarto en promocion vale 88" $ do
             precioFinal (delDia dobleCuarto) `shouldBe` 88
+
+suiteDeTestsParte3 = describe "Algunos cambios mas" $ do  
+    describe "hacerVeggie" $ do
+        it "Dada una hamburguesa con carne y cheddar retorna la hamburguesa en formato vegetariano" $ do
+            hacerVeggie cuartoDeLibra `shouldBe` cuartoDeLibra {ingredientes = [Pan, PatiVegano, QuesoDeAlmendras, Pan]}
+        it "Dada una hamburguesa de carne convertida a vegetariana retorna una hamburguesa con un precio actualizado a los nuevos productos" $ do
+            precioFinal (hacerVeggie cuartoDeLibra) `shouldBe` 49
+    describe "cambiarPanDePati" $ do
+        it "Dada una hamburguesa con pan retorna la hamburguesa con el pan cambiado por pan integral" $ do
+            cambiarPanDePati cuartoDeLibra `shouldBe` cuartoDeLibra {ingredientes = [PanIntegral, Carne, Cheddar, PanIntegral]}
+        it "Dada una hamburguesa con pan integral retorna la hamburguesa con el precio actualizado" $ do
+            precioFinal (cambiarPanDePati cuartoDeLibra) `shouldBe` 56
+    describe "dobleCuartoVegano" $ do
+        it "Un Doble Cuarto Vegano es una doble cuarto con pan integral y con pati vegano" $ do
+            dobleCuartoVegano `shouldBe` dobleCuarto {ingredientes = [PanIntegral, QuesoDeAlmendras, PatiVegano, PatiVegano, QuesoDeAlmendras, PanIntegral]}
+        it "Un Doble Cuarto Vegano tiene su precio actualizado por sus ingredientes" $ do
+            precioFinal dobleCuartoVegano `shouldBe` 76
