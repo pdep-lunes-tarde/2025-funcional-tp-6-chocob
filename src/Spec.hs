87b5f3a64f37bf4aac161f3c80f0a2b3c09d5445
NemeFranco <franconeme@gmail.com>
2025-05-10 15:27:40 -0300

    Correccion primeros tests y creacion setup de pruebas

--- src/Spec.hs
+++ src/Spec.hs
@@ -4,11 +4,24 @@ import Library
 import Test.Hspec
 import Control.Exception (evaluate)
 
+
 correrTests :: IO ()
 correrTests = hspec $ do
+    suiteDeTestsParte1
+    -- suiteDeTestsParte2
+    -- suiteDeTestsParte3
+
+suiteDeTestsParte1 = describe "Hamburguesas" $ do
+    let mcPollo = Hamburguesa {precioBase = 20, ingredientes = [Pan,Pollo,QuesoDeAlmendras,Pan]}
+    
     describe "precioFinal" $ do
         it "El precio final de una hamburguesa es la sumatoria de los precios de los ingredientes + el precio base." $ do
             precioFinal cuartoDeLibra `shouldBe` 54
     describe "agrandar" $ do
-        it "Agregar carne a la hamburguesa" $ do
-            agrandar cuartoDeLibra Carne `shouldBe` Hamburguesa {precioBase = 20, ingredientes = [Carne,Carne,Pan,Cheddar,Pan]}
+        it "Dada una hamburguesa de carne retorna la hamburguesa con una carne adicional en su lista de ingredientes" $ do
+            agrandar cuartoDeLibra `shouldBe` cuartoDeLibra {ingredientes = [Pan,Carne,Carne,Cheddar,Pan]}
+        it "Dada una hamburguesa de pollo retorna la hamburguesa con un pollo adicional en su lista de ingredientes" $ do
+            agrandar mcPollo `shouldBe` mcPollo {ingredientes = [Pan,Pollo, Pollo,QuesoDeAlmendras,Pan]}
+    describe "descuento" $ do
+        it "Dada una hamburguesa y un portentaje retorna la hamburguesa con el precio base actualizado" $ do
+            descuento 10 cuartoDeLibra `shouldBe` cuartoDeLibra {precioBase = 18}
